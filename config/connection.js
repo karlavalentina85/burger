e548eda7aa2367336847ccba61a1d113c14af96d
Blackbird <ksilva.rod@gmail.com>
2020-08-08 15:26:14 -0500

    fixed connection.js to work with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : '',
-    database : 'burger_db'
-});
+var mysql = require("mysql");
 
-connection.connect(function(err) {
-    if (err) throw err;
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else { 
+  connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burger_db"
+    });
+};
+
+connection.connect(err => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3002;
 
 var app = express();
 

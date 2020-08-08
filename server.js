060ab0aacedfe6f60e782ebc6b6f5a6bfe3c70c1
Blackbird <ksilva.rod@gmail.com>
2020-08-08 13:36:36 -0500

    public direct

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-const express = require('express')
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
